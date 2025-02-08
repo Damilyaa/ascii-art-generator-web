94c54482a4cedf5ed72b633f2f3d715960890911
Damilyaa <damilyaamangeldykyzy@gmail.com>
2025-02-08 10:51:16 +0500

    frontend

--- main.go
+++ main.go
@@ -10,22 +10,14 @@ import (
 var templates = template.Must(template.ParseFiles("templates/index.html"))
 
 func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ascii-art", asciiArtHandler)
 
-	/* http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "400 - Bad Request", http.StatusBadRequest)
-	})
-	http.HandleFunc("404", func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
-	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "500 - Bad Request", http.StatusInternalServerError)
-	})
-	*/
-	fmt.Println(":8080")
-
+	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
-
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,9 +28,9 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
-func asciiArtHandle(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -52,12 +44,11 @@ func asciiArtHandle(w http.ResponseWriter, r *http.Request) {
 	banner := r.FormValue("banner")
 
 	if text == "" || banner == "" {
-		http.Error(w, "Bad Request", http.StatusBadGateway)
+		http.Error(w, "Bad Request: Missing text or banner", http.StatusBadRequest)
 		return
 	}
 
 	templatesMap, err := banners.ParseTemplates()
-
 	if err != nil {
 		http.Error(w, "Internal Server Error: cannot load banners", http.StatusInternalServerError)
 		return
@@ -68,21 +59,13 @@ func asciiArtHandle(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not Found: banner not found", http.StatusNotFound)
 		return
 	}
+
 	result, err := tmpl.Execute(text)
 	if err != nil {
 		http.Error(w, "Internal Server Error: failed to generate ASCII", http.StatusInternalServerError)
 		return
 	}
 
-	data := struct {
-		Text   string
-		Banner string
-		Result string
-	}{
-		Text:   text,
-		Banner: banner,
-		Result: result,
-	}
-
-	templates.ExecuteTemplate(w, "index.html", data)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(result))
 }
