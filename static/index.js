8f58bdfce4f1bf992ffcfbf0b89427ed107ab1a7
akabdylk <akabdylk@TCE-12.local>
2025-02-09 23:40:55 +0500

    feat: fixed index.js

--- static/index.js
+++ static/index.js
@@ -2,12 +2,10 @@ document.getElementById("text-input").addEventListener("input", function() {
     const text = document.getElementById("text-input").value;
     const banner = document.getElementById("banner-select").value;
     const output = document.getElementById("ascii-output");
-
     if (!text.trim()) {
         output.textContent = "Please enter text!";
         return;
     }
-
     fetch("/ascii-art", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
